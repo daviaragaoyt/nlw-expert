6786da28ca80bd5738a2a42c7e22dc0132e541ed
aluno <aluno@alunos.uniceplac.edu>
2024-03-11 09:37:51 -0300

    update perguntas

--- script.js
+++ script.js
@@ -4,25 +4,25 @@ const perguntas = [
         respostas: [
             "Uma linguagem de marcação para criar páginas web",
             "Uma linguagem de programação usada para criar interatividade em páginas web",
-            "Um estilo de folha de estilo para tornar as páginas web mais bonitas"
+            "Um estilo de folha de estilo para tornar as páginas web mais bonitas",
         ],
         correta: 1
     },
     {
-        pergunta: "Qual é a função do comando 'console.log()' em JavaScript?",
+        pergunta: "Qual função exerce o 'Console.log()'?",
         respostas: [
             "Exibir um alerta na página",
             "Registrar mensagens de erro no console do navegador",
-            "Imprimir mensagens no console do navegador"
+            "Imprimir mensagens no console do navegador",
         ],
         correta: 2
     },
     {
-        pergunta: "Qual é o operador de atribuição em JavaScript?",
+        pergunta: "Qual deles é de atribuição em Js?",
         respostas: [
             "++",
             "=",
-            "==="
+            "===",
         ],
         correta: 1
     },
@@ -31,16 +31,16 @@ const perguntas = [
         respostas: [
             "Um valor constante que não pode ser alterado",
             "Um nome simbólico para armazenar dados",
-            "Uma função que retorna um valor específico"
+            "Uma função que retorna um valor específico",
         ],
         correta: 1
     },
     {
-        pergunta: "Qual é a função do operador '===' em JavaScript?",
+        pergunta: "O que o '===' faz?",
         respostas: [
             "Comparar dois valores sem levar em conta o tipo de dado",
             "Verificar se dois valores são estritamente iguais em valor e tipo",
-            "Concatenar duas strings"
+            "Concatenar duas strings",
         ],
         correta: 1
     },
@@ -49,43 +49,43 @@ const perguntas = [
         respostas: [
             "Um laço que repete um bloco de código",
             "Um conjunto de instruções que executa uma tarefa específica",
-            "Uma estrutura de decisão que verifica uma condição"
+            "Uma estrutura de decisão que verifica uma condição",
         ],
         correta: 1
     },
     {
-        pergunta: "Qual é a maneira correta de declarar uma variável em JavaScript?",
+        pergunta: "Qual destas esta correta?",
         respostas: [
-            "var x = 10;",
-            "variável x = 10;",
-            "int x = 10;"
+            "var x = 10",
+            "variável x = 10",
+            "int x = 10",
         ],
         correta: 0
     },
     {
-        pergunta: "Qual é a função do método 'addEventListener()' em JavaScript?",
+        pergunta: "Qual é a função do método 'addEventListener()'?",
         respostas: [
             "Remover um evento de um elemento HTML",
             "Adicionar um evento a um elemento HTML",
-            "Modificar o estilo de um elemento HTML"
+            "Modificar o estilo de um elemento HTML",
         ],
         correta: 1
     },
     {
-        pergunta: "O que é uma declaração 'if' em JavaScript?",
+        pergunta: "O que é uma declaração 'if' ?",
         respostas: [
             "Um tipo de loop que executa um bloco de código várias vezes",
             "Uma estrutura de controle de fluxo que executa um bloco de código se uma condição for verdadeira",
-            "Uma função que retorna o maior valor de dois números"
+            "Uma função que retorna o maior valor de dois números",
         ],
         correta: 1
     },
     {
-        pergunta: "Qual é a função do método 'querySelector()' em JavaScript?",
+        pergunta: "Qual é a função do 'querySelector()'?",
         respostas: [
             "Selecionar vários elementos HTML com base em uma classe",
             "Selecionar um único elemento HTML com base em um seletor CSS",
-            "Selecionar elementos HTML com base em seus IDs"
+            "Selecionar elementos HTML com base em seus IDs",
         ],
         correta: 1
     }
@@ -94,9 +94,9 @@ const perguntas = [
 //Variaveis que pegam os valores de quiz e do template
 const quiz = document.getElementById('quiz')
 const template = document.querySelector('template')
-const corretas=new Set()
-const totalPerguntas=perguntas.length
-const mostrarTotal=document.querySelector('#acertos span')
+const corretas = new Set()
+const totalPerguntas = perguntas.length
+const mostrarTotal = document.querySelector('#acertos span')
 
 
 //Looping que roda as 10 perguntas
@@ -116,7 +116,7 @@ for (const item of perguntas) {
             if (estaCorreta) {
                 corretas.add(item)
             }
-            mostrarTotal.textContent=corretas.size+'de'+totalPerguntas
+            mostrarTotal.textContent = corretas.size + 'de' + totalPerguntas
         }
 
 
